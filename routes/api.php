51cdbc71f29dd97439759360b457a1bde42e6165
Achal1218 <achalbhujbal2003@gmail.com>
2024-04-23 18:23:07 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -25,23 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1/admin', 'as' => 'v1/admin'], function () {
 
-    //FAQ
-    Route::post('createFaq',[FaqController::class,'createFaq']);
-    Route::post('updateFaq',[FaqController::class,'updateFaq']);
-    Route::get('getFaq',[FaqController::class,'getFaq']);
-    Route::delete('deleteFaq',[FaqController::class,'deleteFaq']);
-    Route::get('getFaqById',[FaqController::class,'getFaqById']);
     
-    //contactus
-    Route::post('addContactUs',[ContactUsController::class,'addContactUs']);
-    Route::get('getAllContactUs',[ContactUsController::class,'getAllContactUs']);
-
-    //News
-    Route::post('createNews',[NewsController::class,'createNews']);
-    Route::post('updateNews',[NewsController::class,'updateNews']);
-    Route::get('getAllNews',[NewsController::class,'getAllNews']);
-    Route::delete('deleteNews',[NewsController::class,'deleteNews']);
-    Route::get('getNewsById',[NewsController::class,'getNewsById']);
 
 
 
